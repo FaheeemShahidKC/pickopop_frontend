928d105e9189eb5d0763cd8eb5dda71c6f78165f
Faheem Shahid <faymoone@gmail.com>
2024-04-01 21:15:11 +0530

    set header, main home, login

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Login from './components/user/login/Login'
+import Sample from './pages/Sample'
+import HomePage from './pages/user/HomePage'
+
+function App() {
+
+  return (
+    <>
+      <HomePage></HomePage>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/user/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainHome from '../../components/user/mainHome/MainHome'
+import Header from '../../components/user/header/Header'
+
+function HomePage() {
+  return (
+    <>
+    <Header></Header>
+    <MainHome></MainHome>
+    </>
+  )
+}
+
+export default HomePage
